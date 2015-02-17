fbd324b4eb9a760b95af9a00e051be590d966b6e
sibchil <sibchilksa@gmail.com>
2015-02-17 16:06:34 +0000

    initial project structure

---
+++ src/org/sibsystem/game/Game.java
@@ -0,0 +1,11 @@
+package org.sibsystem.game;
+
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.LWJGLException;
+ 
+public class Game {
+	public static void main(String[] args) {
+		/* game code here */
+	}
+}
